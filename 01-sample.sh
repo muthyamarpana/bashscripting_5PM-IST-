856ec8cbbb7ccf5d4fe458423490cce59dd7858d
ArpanaM <muriki.arpana@gmail.com>
2024-11-18 17:41:04 +0530

    Updated a file

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo Hello World!
+echo -e "\e[31mHello in Red Color\e[0m"
+echo -e "\e[32mHello in Green Color\e[0m"
+echo -e "\e[33mHello in Yellow Color\e[0m"
+echo -e "\e[34mHello in Blue Color\e[0m"
+echo -e "\e[35mHello in Magenta Color\e[0m"
+echo -e "\e[31mHello in Cyan Color\e[0m"
