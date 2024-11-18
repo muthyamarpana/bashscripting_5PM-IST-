9b62dc3af579434d638d18efad44879fb88d0e42
ArpanaM <muriki.arpana@gmail.com>
2024-11-18 17:50:53 +0530

    Variables

--- variables.sh
+++ variables.sh
@@ -4,3 +4,13 @@ echo a=${a}
 echo a in currency = ${a}USD
 DATE=2025-11-18
 echo Good Morning, Welcome to Bash Scripting, Todays Date is ${DATE}
+
+# Scalar variables
+
+d=(10 20 30 40 50)
+echo ${d[0]}
+echo ${d[1]}
+echo ${d[2]}
+echo ${d[3]}
+echo ${d[4]}
+
